53c5f6163dee607843cb39742ee00923753b357a
Michael Pollind <mpollind@gmail.com>
2021-09-12 21:19:06 -0700

    update glew and Devil

--- HPL2/3rdparty/cmake/glew.cmake
+++ HPL2/3rdparty/cmake/glew.cmake
@@ -2,7 +2,9 @@ ExternalProject_Add(external_GLEW
         GIT_REPOSITORY https://github.com/nigels-com/glew.git
         PREFIX ${BUILD_DIR}/GLEW
         GIT_TAG        ${libGLEW_Version}
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${LIBDIR}/GLEW
-        SOURCE_DIR build/cmake
+        BUILD_IN_SOURCE TRUE
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ${MAKE_EXECUTABLE} extensions; ${MAKE_EXECUTABLE}
+        INSTALL_COMMAND ${MAKE_EXECUTABLE} install DESTDIR=${LIBDIR}/GLEW GLEW_DEST=/
         INSTALL_DIR ${LIBDIR}/GLEW
         )
