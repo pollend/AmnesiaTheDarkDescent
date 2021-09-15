520c4a0cba3fee0542afe4e896aff8a4b01ad0cb
Michael Pollind <mpollind@gmail.com>
2021-09-14 21:45:07 -0700

    feat: add theora

--- build_environment/cmake/theora.cmake
+++ build_environment/cmake/theora.cmake
@@ -1,7 +1,20 @@
 ExternalProject_Add(external_theora
         URL http://downloads.xiph.org/releases/theora/libtheora-${libtheora_Version}.tar.bz2
         PREFIX ${BUILD_DIR}/theora
+        BUILD_IN_SOURCE TRUE
         DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}/downloads
+        CONFIGURE_COMMAND ./configure 
+                --with-vorbis="${BUILD_DIR}/vorbis"
+                --with-ogg="${BUILD_DIR}/ogg"
+                --disable-examples 
+                --disable-shared 
+                --disable-sdltest   
+                --disable-vorbistest
+                --disable-spec 
+                --prefix ${LIBDIR}/theora 
+        BUILD_COMMAND make
+        INSTALL_COMMAND ${MAKE_EXECUTABLE} install
         INSTALL_DIR ${LIBDIR}/theora
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${LIBDIR}/theora
         )
+
+add_dependencies(external_theora external_ogg external_vorbis)
